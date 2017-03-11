22bf0dae516f3960f91008b04baf28d409ce6802
p14para <p14para@ionio.gr>
2017-03-11 18:19:29 +0200

    Update TweetBeacon.js

--- public/javascripts/TweetBeacon.js
+++ public/javascripts/TweetBeacon.js
@@ -20,7 +20,7 @@ if (tweet.sentiment.score < 0) {
   this.color = "blue";
   }
   else if (tweet.sentiment.score > 0) {
-    this.color = 0xDDDD00;
+    this.color = "orange";
 }
   
   this.addBeam();
