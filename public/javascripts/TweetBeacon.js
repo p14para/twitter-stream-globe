e23fa7204f92bb9015cca43ee9bc5f4ac205feb8
p14para <p14para@ionio.gr>
2017-03-13 10:39:54 +0200

    Update TweetBeacon.js

--- public/javascripts/TweetBeacon.js
+++ public/javascripts/TweetBeacon.js
@@ -15,14 +15,23 @@ TweetBeacon = function(tweet) {
   this.add(this.container);
 
   // Set base color depending on sentiment score
-  this.color = 0xFFFFFF;
+   this.color = 0xFFFFFF;
 
-  if (tweet.sentiment.score < 0) {
-  this.color = 0xFF0000;
+  
+  if (tweet.sentiment.score < -0.5) {
+  this.color = 0xFF3300;
   }
-  else if (tweet.sentiment.score > 0) {
-    this.color = 0xDDDD00;
+  else if (tweet.sentiment.score < 0) {
+    this.color = 0xFFFF00;
   }
+  else if (tweet.sentiment.score < 0.5){
+    this.color = 0x006633;
+  }
+  else { 
+    this.color = 0x0066FF; }
+
+
+ 
 
   this.addBeam();
   this.addShockwave();
