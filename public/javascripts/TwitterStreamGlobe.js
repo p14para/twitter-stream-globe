60b936f001f3f36671094242d194d0ada6ad4d1c
p14para <p14para@ionio.gr>
2017-05-07 18:35:23 +0300

    Update TwitterStreamGlobe.js

--- public/javascripts/TwitterStreamGlobe.js
+++ public/javascripts/TwitterStreamGlobe.js
@@ -163,7 +163,7 @@
 	 */ 
 	function render () {
 
-		earthMesh.rotation.y = earthMesh.rotation.y + 0.0010;
+		earthMesh.rotation.y = earthMesh.rotation.y + 0.010;
 		
 	  renderer.autoClear = false;
 	  renderer.clear();
